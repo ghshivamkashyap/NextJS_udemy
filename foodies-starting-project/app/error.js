47b42b2c75fc486a74b8ca3356b90727dabcfe51
shivam digiprod <lootforecast@gmail.com>
2024-11-19 22:40:14 +0530

    Error page done(default by next.js)

---
+++ foodies-starting-project/app/error.js
@@ -0,0 +1,16 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+const Error = (data) => {
+  console.log('Error', data);
+  
+  return (
+    <div className=" flex flex-col">
+      <div>Something went wrong</div>
+      <Link href="/">Go back to home</Link>
+    </div>
+  );
+};
+
+export default Error;
