4d9e7ef7e53ed2f093316435a9b4275e269cb1a3
shivam digiprod <lootforecast@gmail.com>
2024-12-14 21:08:40 +0530

    fetch - cache property (force-cash - default in next jks, ''no-cash'  - to remove next js aggressive caching)

--- caching_project/app/messages/layout.js
+++ caching_project/app/messages/layout.js
@@ -1,8 +1,6 @@
 export default async function MessagesLayout({ children }) {
   const response = await fetch("http://localhost:8080/messages", {
-    headers: {
-      "X-ID": "layout",
-    },
+    cache: "no-store",
   });
   const messages = await response.json();
   const totalMessages = messages.length;
