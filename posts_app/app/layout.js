d84f900201d15b81d95608ad3e9fb31c0b938922
shivam digiprod <lootforecast@gmail.com>
2024-12-15 14:22:35 +0530

    layout metadata done - (for layout based metadat, default for all if no metadata for another page exists)

--- posts_app/app/layout.js
+++ posts_app/app/layout.js
@@ -2,7 +2,7 @@ import Header from '@/components/header';
 import './globals.css';
 
 export const metadata = {
-  title: 'NextPosts',
+  title: 'Default metadata title',
   description: 'Browse and share amazing posts.',
 };
 
