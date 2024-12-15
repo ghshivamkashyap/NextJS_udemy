cd5133a6686e7c1f2c675044e39309bfbf432f68
shivam digiprod <lootforecast@gmail.com>
2024-12-15 14:20:13 +0530

    dynami metadata done

--- posts_app/app/feed/page.js
+++ posts_app/app/feed/page.js
@@ -1,5 +1,13 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  return {
+    title: `All  ${posts.length} posts by all users`,
+    description: "See what everyone has been up to.",
+  };
+}
 
 export default async function FeedPage() {
   const posts = await getPosts();
