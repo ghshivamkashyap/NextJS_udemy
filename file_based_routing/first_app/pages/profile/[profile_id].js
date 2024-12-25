a69b3b3ba3bc4a282dfcc9b61882d884f66ff6fc
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-12-25 22:31:14 +0530

    geting data from query dynamic path id

--- file_based_routing/first_app/pages/profile/[profile_id].js
+++ file_based_routing/first_app/pages/profile/[profile_id].js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const UserProfile = () => {
+  const router = useRouter();
+
+  console.log("router: ", router);
+
   return (
     <div>
       <h1>User Profile Page</h1>
