6eda790e64ccc01426a7f62b46692aa5473300fc
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-12-25 22:41:23 +0530

    3 way nested routing for nested pages

---
+++ file_based_routing/first_app/pages/clients/[id]/client_project.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClientProject = () => {
+  return (
+    <div>
+      <h1>Client Project Page</h1>
+    </div>
+  );
+};
+
+export default ClientProject;
---
+++ file_based_routing/first_app/pages/clients/[id]/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClientProfile = () => {
+  return (
+    <div>
+        <h1>Client Profile Page</h1>
+    </div>
+  );
+}
+
+export default ClientProfile;
