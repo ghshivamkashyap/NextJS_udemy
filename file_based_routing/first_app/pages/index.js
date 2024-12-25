33bf015056285840f088e4d21f61eefa480c7c82
SHIVAM KASHYAP <lootforecast@gmail.com>
2024-12-25 23:01:11 +0530

    navigation done

--- file_based_routing/first_app/pages/index.js
+++ file_based_routing/first_app/pages/index.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Link from "next/link";
 const HomePage = () => {
   return (
     <>
       <h1 className=" text-3xl">Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/blogs/s654e5g">Blogs</Link>
+        </li>
+      </ul>
     </>
   );
 };
